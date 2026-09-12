3f7a50dce77da896ada3cbb16543c2094a5002f4
Yana <yanakalinichenko2503@gmail.com>
2026-09-12 17:53:15 +0200

    add assertJ

--- src/test/java/restful_booker/CrudTest.java
+++ src/test/java/restful_booker/CrudTest.java
@@ -2,11 +2,12 @@ package restful_booker;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 
 public class CrudTest {
     @Test
     public void test() {
-        assertTrue(true);
+        assertThat(true).isTrue();
     }
 }
